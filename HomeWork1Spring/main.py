b65c1bbf695f8315fc836eb833252b00a08619f8
Denzyyyy <106283525+Denzyyyy@users.noreply.github.com>
2024-01-27 21:41:45 -0600

    Add files via upload

--- HomeWork1Spring/main.py
+++ HomeWork1Spring/main.py
@@ -1,4 +1,20 @@
-#Hugues Izah, Student id 2310365
-userNum = int(input())
-userNumSquared = userNum * userNum
-print(userNumSquared)
+# Hugues Izah, student ID: 2310365
+print("Birthday Calculator")
+
+current_month = int(input("Current Month: "))
+current_day = int(input("Day: "))
+current_year = int(input("Year: "))
+
+print("Birthday")
+birthday_month = int(input("Month: "))
+birthday_day = int(input("Day: "))
+birthday_year = int(input("Year: "))
+
+age = current_year - birthday_year
+if (current_month, current_day) < (birthday_month, birthday_day):
+    age -= 1
+
+print(f"\nYou are {age} years old.")
+
+if current_month == birthday_month and current_day == birthday_day:
+    print("Happy Birthday!")
