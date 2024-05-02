8818f4544be7cd9aba86998449bdefb0c4b8f560
Denzyyyy <106283525+Denzyyyy@users.noreply.github.com>
2024-05-01 21:30:24 -0500

    Add files via upload

---
+++ Homework4Spring/zylab1411.py
@@ -0,0 +1,16 @@
+def selection_sort_descend_trace(integer_list):
+    n = len(integer_list)
+    for i in range(n-1):
+        max_index = i
+        for j in range(i+1,n):
+            if(integer_list[j]>integer_list[max_index]):
+                max_index = j
+        integer_list[i], integer_list[max_index] = integer_list[max_index],integer_list[i]
+        for i in integer_list:
+            print(i, end=' ')
+        print()
+
+
+if __name__ == "__main__":
+    list_of_ints = [int(i) for i in input().split()]
+    selection_sort_descend_trace(list_of_ints)
